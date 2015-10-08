09166f8a10c5fe3ea54f21163f3b0b603e587d7e
Fredrik Landström <fredrik.landstrom@compose.se>
2015-10-08 06:59:56 +0200

    initial commit of tests

---
+++ test/indexSpec.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = require('chai').expect;
+describe('Omnibus', function() {
+	it('connects to a database',function(){
+		expect('Hello').to.equal('Hello');
+	});
+	it('insert to a database',function(){
+		expect('Hello').to.equal('Hello');
+	});
+});
