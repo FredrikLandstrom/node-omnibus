88a3d1b2715fa6950bfa1a7533481305108da60b
Fredrik Landström <fredrik.landstrom@compose.se>
2015-10-10 17:09:00 +0200

    updated tests

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -2,7 +2,8 @@ var chai = require('chai');
 var expect = require('chai').expect;
 var omnibus = require('../index.js');
 var sinon = require('sinon');
-var sinonChai('sinon-chai');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
 describe('Omnibus', function() {
 	it('connects to a database',function(){
@@ -13,11 +14,30 @@ describe('Omnibus', function() {
 	});
 });
 
-describe('SQL Formatting tests', function(){
+describe('queryBuilder tests', function(){
 
+	before(function(){
+		var omnibusConnection = omnibus.createConnection({
+			host  		: 'primarywebserver', 
+			port 		: '8080',
+			user 		: 'root', 
+			password	: ''
+		});
+	})
 
-
-	it('can handle * as column definition', function(){
-		expect(omnibus.Connection.querybuilder('select * from alerts.status').to.equal('test'));
+	it('correctly formats SELECT sql queries into JSON objects', function(){
+		var omnibusConnection = omnibus.createConnection({});
+		var restOptions = omnibusConnection.queryBuilder('select * from alerts.status where Serial > 1 and Serial < 10 order by Serial');
+		expect(restOptions.method).to.equal('GET');
+		expect(restOptions.path).to.equal('alerts/status');
+		expect(restOptions.filter).to.equal(encodeURI('Serial > 1 and Serial < 10'));
+		expect(restOptions.collist).to.equal('');
+		expect(restOptions.orderBy).to.equal(encodeURI('Serial'));
+		var restOptions = omnibusConnection.queryBuilder('select Node,Serial from alerts.status where Serial > 1 and Serial < 10 order by Serial');
+		expect(restOptions.method).to.equal('GET');
+		expect(restOptions.path).to.equal('alerts/status');
+		expect(restOptions.filter).to.equal(encodeURI('Serial > 1 and Serial < 10'));
+		expect(restOptions.collist).to.equal('Node,Serial');
+		expect(restOptions.orderBy).to.equal(encodeURI('Serial'));
 	});
 })
