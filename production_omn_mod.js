63761edf3b808e43490b045ab8eb8d33a04b85df
Vuk Kadenic <vuk.kadenic@gmail.com>
2016-03-17 21:10:20 +0100

    Configuration file and related dependencies are fixed. All of the configuration is now read from a single conf file

---
+++ production_omn_mod.js
@@ -0,0 +1,22 @@
+var production_omn_mod= {
+    /* PostgreSQL Database configuration */
+    db: {
+        name: "********",
+        password: "********",
+        username: "********",
+        host: "********",
+        port: 5432
+    },
+    /* Omnibus Database configuration */
+    db_omnibus: {
+        name: "****",
+        password: "****",
+        username: "****",
+        host: "***.***.***.***",
+        REST_port: "*******"
+    },
+    /* Omnibus authorization (root/root) */
+    omni_authorization: 'Basic cm9vdDpyb290'
+};
+
+module.exports = production_omn_mod;
