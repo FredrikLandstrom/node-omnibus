a4a5d65f241f5f1f9abb4ba4c39163c13c5707f0
Fredrik Landström <fredrik.landstrom@compose.se>
2016-03-26 07:25:59 +0100

    updated sql factory to have callback to main

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 exports.createConnection = function createConnection(config) {
 	var Connection 			= require('./lib/Connection');
 	var ConnectionConfig	= require('./lib/ConnectionConfig');
-
 	return new Connection({config: new ConnectionConfig(config)});
 }
-
