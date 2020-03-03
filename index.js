b387378e89e1053898a987fee6cd50e6ee5b8878
Fredrik Landström <fredrik.j.landstrom@gmail.com>
2020-03-03 20:38:05 +0100

    removed old entrypoind index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-exports.createConnection = function createConnection(config) {
-	var Connection 			= require('./lib/Connection');
-	var ConnectionConfig	= require('./lib/ConnectionConfig');
-	return new Connection({config: new ConnectionConfig(config)});
-}
